8f3190ba3c95c44472207bc0f6a5e5aabfd72b3a
mogi86 <nobuyuki.mogi@gmail.com>
2022-06-14 15:19:38 +0900

    add sample component for client side rendering

---
+++ components/Part.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Part Components!</p>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+
+export default {
+  setup () {
+    onMounted(() => {
+      console.log(window.innerWidth)
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Index page</h1>
+  <!-- rendering on client side -->
+  <ClientOnly>
+    <part></part>
+  </ClientOnly>
+</template>
+
+<script>
+import Part from "../components/Part";
+export default {
+  components: {Part}
+}
+</script>
